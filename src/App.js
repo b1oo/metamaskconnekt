31954aa935d00224b23cf21f42597f40a46f8ac6
Sergio <sergiobluez@gmail.com>
2023-03-19 14:01:09 -0700

    Updated to function properly

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Web3 from "web3";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [account, setAccount] = useState("");
+
+  useEffect(() => {
+    connectToMetaMask();
+  }, []);
+
+  const connectToMetaMask = async () => {
+    if (window.ethereum) {
+      try {
+        // Request account access
+        await window.ethereum.request({ method: "eth_requestAccounts" });
+
+        // Initialize web3
+        const web3 = new Web3(window.ethereum);
+
+        // Get user's account
+        const accounts = await web3.eth.getAccounts();
+        setAccount(accounts[0]);
+      } catch (error) {
+        console.error("Error connecting to MetaMask:", error);
+      }
+    } else {
+      alert("Please install MetaMask!");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <h1>MetaMask React App</h1>
+      {account ? (
+        <p className="connected-account">Connected Account: {account}</p>
+      ) : (
+        <p className="not-connected">Not connected</p>
+      )}
     </div>
   );
-}
+};
 
 export default App;
